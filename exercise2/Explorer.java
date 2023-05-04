18c681d2b232ac0f5c6b5f0a39e797e6a0c5ed02
Justinn Acedera <125964343+Kawaki11@users.noreply.github.com>
2023-05-04 14:52:35 +0800

    Update Explorer.java

--- exercise2/Explorer.java
+++ exercise2/Explorer.java
@@ -1,1 +1,39 @@
+package Explorers;
 
+import Planets.Earth;
+import Planets.Mars;
+import Planets.Mercury;
+import Planets.Planet;
+import Planets.Saturn;
+import Planets.Uranus;
+import Planets.Jupiter;
+import Planets.Neptune;
+import Planets.Venus;
+import Planets.Pluto;
+
+
+public interface Explorer {
+
+    void visit(Mars mars);
+
+    void visit(Saturn saturn);
+
+    void visit(Mercury mercury);
+
+    void visit(Neptune neptune);
+
+    void visit(Earth earth);
+
+    void visit(Venus venus);
+
+    void visit(Jupiter jupiter);
+
+    void visit(Uranus uranus);
+
+    void visit(Pluto pluto);
+
+    default void visit(Planet planet) {
+
+        System.out.println("Landed on the planet");
+    }
+}
